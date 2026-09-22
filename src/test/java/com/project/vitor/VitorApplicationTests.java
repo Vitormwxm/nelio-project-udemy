77b5e97fd9e03669ad210b1fa213fc4740a7cb31
Vitor Martiniano <vitormwxm@gmail.com>
2026-09-21 21:17:10 -0300

    first commit

---
+++ src/test/java/com/project/vitor/VitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
