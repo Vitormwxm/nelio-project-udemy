e4a3eedcfd295320b8022da01791f8b038159cf8
Vitor Martiniano <vitormwxm@gmail.com>
2026-09-21 22:37:38 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/project/vitor/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.project.vitor.repositories;
+
+import com.project.vitor.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
