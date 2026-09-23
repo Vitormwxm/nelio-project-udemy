483c8641660eee7b83de27f1fdfd6dbc4c5d8d7f
Vitor Martiniano <vitormwxm@gmail.com>
2026-09-22 21:12:53 -0300

    OrderStatus ENUM

--- src/main/java/com/project/vitor/entities/Order.java
+++ src/main/java/com/project/vitor/entities/Order.java
@@ -2,6 +2,7 @@ package com.project.vitor.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.vitor.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
@@ -18,6 +19,8 @@ public class Order {
     @JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
@@ -25,9 +28,10 @@ public class Order {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User user) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User user) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.user = user;
     }
 
@@ -45,6 +49,16 @@ public class Order {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getUser() {
